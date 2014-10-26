7982b4a2607f98dec14d21937e57178fa48877d0
mkolisnyk <kolesnik.nickolay@gmail.com>
2014-10-26 22:37:28 +0000

    Draft readers/writers were added

---
+++ src/main/java/com/github/mkolisnyk/aerial/readers/AerialJiraReader.java
@@ -0,0 +1,52 @@
+/**
+ * .
+ */
+package com.github.mkolisnyk.aerial.readers;
+
+import com.github.mkolisnyk.aerial.AerialReader;
+
+/**
+ * @author Myk Kolisnyk
+ *
+ */
+public class AerialJiraReader implements AerialReader {
+
+    /**
+     * .
+     */
+    public AerialJiraReader() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.mkolisnyk.aerial.AerialReader#open(java.lang.Object[])
+     */
+    public void open(Object... params) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.mkolisnyk.aerial.AerialReader#close()
+     */
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.mkolisnyk.aerial.AerialReader#readNext()
+     */
+    public String readNext() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.mkolisnyk.aerial.AerialReader#hasNext()
+     */
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}

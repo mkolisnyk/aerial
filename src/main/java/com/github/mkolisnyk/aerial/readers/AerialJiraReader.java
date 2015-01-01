cae2a40ea6def3ba0851d37ebffb806b0014c0d2
mkolisnyk <kolesnik.nickolay@gmail.com>
2015-01-01 22:56:29 +0000

    #5 Initial Jira reader implementation was added

--- src/main/java/com/github/mkolisnyk/aerial/readers/AerialJiraReader.java
+++ src/main/java/com/github/mkolisnyk/aerial/readers/AerialJiraReader.java
@@ -3,6 +3,19 @@
  */
 package com.github.mkolisnyk.aerial.readers;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.SearchRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueField;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.util.concurrent.Promise;
 import com.github.mkolisnyk.aerial.AerialReader;
 
 /**
@@ -11,42 +24,76 @@ import com.github.mkolisnyk.aerial.AerialReader;
  */
 public class AerialJiraReader implements AerialReader {
 
+    private String userName;
+    private String password;
+    private String url;
+    private String fieldName;
+
     /**
      * .
      */
-    public AerialJiraReader() {
-        // TODO Auto-generated constructor stub
+    private List<String> content;
+    /**
+     * .
+     */
+    private Iterator<String> iterator;
+
+    /**
+     * .
+     */
+    public AerialJiraReader(String urlValue, String userNameValue, String passwordValue, String fieldNameValue) {
+        this.userName = userNameValue;
+        this.password = passwordValue;
+        this.url = urlValue;
+        this.fieldName = fieldNameValue;
     }
 
     /* (non-Javadoc)
      * @see com.github.mkolisnyk.aerial.AerialReader#open(java.lang.Object[])
      */
     public void open(Object... params) throws Exception {
-        // TODO Auto-generated method stub
-
+        JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+        URI uri = new URI(url);
+        JiraRestClient client = factory.createWithBasicHttpAuthentication(uri, userName, password);
+        SearchRestClient searchClient = client.getSearchClient();
+        content = new ArrayList<String>();
+        for (Object query : params) {
+            Promise<SearchResult> result = searchClient.searchJql((String) query);
+            Iterable<Issue> issues = result.get().getIssues();
+            for (Issue issue : issues) {
+                IssueField field = issue.getField(fieldName);
+                content.add((String) field.getValue());
+            }
+        }
+        this.iterator = content.iterator();
+        client.close();
     }
 
     /* (non-Javadoc)
      * @see com.github.mkolisnyk.aerial.AerialReader#close()
      */
-    public void close() throws Exception {
-        // TODO Auto-generated method stub
-
+    public final void close() throws Exception {
+        if (content != null) {
+            content.clear();
+            content = null;
+        }
+        iterator = null;
     }
 
     /* (non-Javadoc)
      * @see com.github.mkolisnyk.aerial.AerialReader#readNext()
      */
-    public String readNext() throws Exception {
-        // TODO Auto-generated method stub
+    public final String readNext() {
+        if (hasNext()) {
+            return iterator.next();
+        }
         return null;
     }
 
     /* (non-Javadoc)
      * @see com.github.mkolisnyk.aerial.AerialReader#hasNext()
      */
-    public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
+    public final boolean hasNext() {
+        return iterator != null && iterator.hasNext();
     }
 }

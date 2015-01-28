ad46a3e2314e0cc563ee9af7c92a77d6b9877f2b
mkolisnyk <kolesnik.nickolay@gmail.com>
2015-01-28 23:05:18 +0000

    #18: initial class for GitHub input source as added

---
+++ src/main/java/com/github/mkolisnyk/aerial/readers/AerialGitHubReader.java
@@ -0,0 +1,37 @@
+package com.github.mkolisnyk.aerial.readers;
+
+import com.github.mkolisnyk.aerial.AerialReader;
+import com.github.mkolisnyk.aerial.core.params.AerialParams;
+
+public class AerialGitHubReader extends AerialReader {
+
+    public AerialGitHubReader(AerialParams params) {
+        super(params);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void open(AerialParams params) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String readNext() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
